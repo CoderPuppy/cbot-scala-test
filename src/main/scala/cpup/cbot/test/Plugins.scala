6c275ea57293b346c73d9c8b9f63f7f6cb36fdf8
CoderPuppy <cpup@cpup-laptop>
2014-04-25 23:16:48 -0400

    Finish up SavingPlugin

--- src/main/scala/cpup/cbot/test/Plugins.scala
+++ src/main/scala/cpup/cbot/test/Plugins.scala
@@ -1,14 +1,21 @@
 package cpup.cbot.test
 
-import cpup.cbot.plugin.Plugin
+import cpup.cbot.plugin.{PluginManager, Plugin}
 import com.google.common.eventbus.Subscribe
 import cpup.cbot.plugin.CommandPlugin.{TCommandEvent, TCommandCheckEvent}
 import cpup.cbot.users._
-import cpup.cbot.events.channel.{LeaveEvent, JoinEvent, ChannelMessageEvent, ChannelEvent}
+import cpup.cbot.events.channel._
 import cpup.cbot.channels.{ChannelWrites, Channel}
-import java.io.File
-import play.api.libs.json.{Json, JsArray}
-import cpup.cbot.events.RegisterEvent
+import java.io.{PrintWriter, File}
+import play.api.libs.json.{JsValue, Json, JsObject}
+import cpup.cbot.CBot
+import cpup.cbot.events.user._
+import cpup.cbot.events.channel.ChannelMessageEvent
+import cpup.cbot.events.user.RegisterEvent
+import cpup.cbot.events.channel.JoinEvent
+import cpup.cbot.events.channel.ChannelUpdateEvent
+import cpup.cbot.events.channel.LeaveEvent
+import scala.io.Source
 
 class UsersPlugin extends Plugin {
 	@Subscribe
@@ -352,27 +359,137 @@ class SayPlugin extends Plugin {
 class SavingPlugin(val file: File) extends Plugin {
 	var channels = Json.obj()
 	var users = Json.obj()
+	var nickServUsers = Json.obj()
 
 	implicit val channelWrites = ChannelWrites
 	implicit val userWrites = UserWrites
 
+	def save {
+		val json = Json.prettyPrint(Json.obj(
+			"channels" -> channels,
+			"users" -> users,
+			"nickServUsers" -> nickServUsers
+		))
+		val writer = new PrintWriter(file)
+		writer.write(json)
+		writer.close
+	}
+
+	def updateChannel(chan: Channel) {
+		channels -= chan.name
+		channels += (chan.name -> Json.toJson(chan))
+	}
+
+	def updateUser(user: User) {
+		users -= user.username
+		users += (user.username -> Json.toJson(user))
+	}
+
+	override def enable(manager: PluginManager) = {
+		if(!managers.isEmpty) {
+			throw new AlreadyRegisteredException("already registered")
+		}
+
+		manager match {
+			case bot: CBot =>
+				super.enable(manager)
+				channels = Json.toJson(bot.channels.current.map((chan) => {
+					(chan.name, chan)
+				}).toMap).asInstanceOf[JsObject]
+				users = Json.toJson(bot.users.registeredUsers.toMap).asInstanceOf[JsObject]
+				nickServUsers = Json.toJson(bot.users.nickServUsers.toMap.map((kv) => {
+					(kv._1, kv._2.username)
+				})).asInstanceOf[JsObject]
+
+			case _ =>
+				throw new ClassCastException("SavingPlugin only works for CBots")
+		}
+	}
+
+	override def disable(manager: PluginManager) = {
+		super.disable(manager)
+		channels = Json.obj()
+		users = Json.obj()
+	}
+
 	@Subscribe
 	def join(e: JoinEvent) {
-		channels += (e.channel.name -> Json.toJson(e.channel))
+		updateChannel(e.channel)
+		save
 	}
 
 	@Subscribe
 	def leave(e: LeaveEvent) {
-		channels - e.channel.name
+		channels -= e.channel.name
+		save
+	}
+
+	@Subscribe
+	def channelUpdate(e: ChannelUpdateEvent) {
+		updateChannel(e.channel)
+		save
+	}
+
+	@Subscribe
+	def userUpdate(e: UserUpdateEvent) {
+		updateUser(e.user)
+		save
+	}
+
+	@Subscribe
+	def registerNickServ(e: RegisterNickServEvent) {
+		nickServUsers += (e.nickserv -> Json.toJson(e.user.username))
 	}
 
 	@Subscribe
 	def register(e: RegisterEvent) {
-		users += (e.user.username -> Json.toJson(e.user))
+		updateUser(e.user)
+		save
 	}
 
+	// TODO: use this if unregistering is implemented
 //	@Subscribe
 //	def unregister(e: UnregisterEvent) {
 //		users -= e.user.username
+//		save
 //	}
+}
+object SavingPlugin {
+	def load(bot: CBot, file: File) {
+		val json: JsValue = Json.parse({
+			val source = Source.fromFile(file)
+			val contents = source.mkString
+			source.close
+			contents
+		})
+
+		(json \ "channels").as[Map[String, JsObject]].values.foreach((json) => {
+			bot.channels.join(
+				(json \ "name").as[String],
+				(json \ "key").validate[String].getOrElse(null)
+			).setRejoin((json \ "rejoin").as[Boolean])
+		})
+
+		(json \ "users").as[Map[String, JsObject]].values.foreach((userJSON) => {
+			val user = bot.users.register(
+				(userJSON \ "username").as[String],
+				(userJSON \ "password").validate[String].getOrElse(null)
+			)
+
+			(userJSON \ "permissions").as[List[String]].foreach((perm) => {
+				user.grantPermission(Symbol(perm))
+			})
+
+			(userJSON \ "channelPermissions").as[Map[String, List[String]]].foreach((kv) => {
+				val (chan, perms) = kv
+				perms.foreach((perm) => {
+					user.grantPermission(chan, Symbol(perm))
+				})
+			})
+		})
+
+		(json \ "nickServUsers").as[Map[String, String]].foreach((kv) => {
+			bot.users.registerNickServ(kv._1, bot.users(kv._2))
+		})
+	}
 }
